3557e124c5136a2a1dabac151c9ed24bfc3b57ac
Pravin Nalawade <thepravin28@gmail.com>
2025-10-30 20:32:02 +0530

    Add : Variables in go

---
+++ 01_variables/01_variables.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "Pravin"
+	var name2 = "Pravin 2"
+	name3 := "Pravin 3"
+	fmt.Println(name)
+	fmt.Println(name2)
+	fmt.Println(name3)
+
+	var money int = 14
+	var money2 = 15
+	money3 := 16
+	fmt.Println(money)
+	fmt.Println("money 2 :", money2)
+	fmt.Println("money 3 :", money3)
+
+	var dimension float64 = 89.96
+	dimension2 := 34.56
+	fmt.Println(dimension)
+	fmt.Println(dimension2)
+
+	var isTrue bool = true
+	isFalse := false
+	fmt.Println(isTrue)
+	fmt.Println(isFalse)
+
+	const pi = 3.14
+	fmt.Println(pi)
+
+}
